77d71fb56220e9116acf582c65684c04ce8f574c
vbutiu <victor@reea.net>
2016-03-17 12:42:29 +0200

    pushing index
    pushing index

---
+++ index.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+<style>
+body.custom-background {
+background-color: #ffffff;
+background-image: 
+url('http://seldar.lejer.ro/wp-content/uploads/2013/02/landscapes-hdr_00294274.jpg');
+background-repeat: repeat-y;
+background-position: top center;
+background-attachment: fixed;
+}
+#apps {
+font-family: Helvetica, Arial, Sans-Serif;
+font-size: 16px;
+font-weight: normal;
+font-style: normal;
+line-height: 1.4;
+color: #333;
+background-color: #fff;
+padding: 10px;
+border: 1px solid #eee;
+border-color: rgba(0,0,0,0.2);
+box-shadow: 1px 1px 5px rgba(0,0,0,0.1);
+border-radius: 2px;
+margin: 10px 0;
+}
+a {
+color: #2b82ad;
+text-decoration: none;
+}
+</style>
+<title>Victor Teodor Butiu</title>
+</head>
+<body class="custom-background" >
+<script src="//about.me/embed/butiu.victor"></script>
+<br />
+<div id="apps" class="aboutme-embed 
+aboutme-default aboutme-layout-horizontal" style="visibility: inherit; 
+display: block;"><div xmlns="http://www.w3.org/1999/xhtml" 
+class="aboutme-content"><div class="aboutme-text"><div 
+class="aboutme-name">Apps & Work</div><div 
+class="aboutme-headline">Some aplications I have been used/worked on 
+during time.
+</div>
+<div class="aboutme-apps"><a 
+href="http://www.new-dmoz.org" 
+class="app-link" title="new-Dmoz" 
+target="_top">&nbsp;new-Dmoz.Org</a>&nbsp;Personal Directory Project, 
+made 
+in 2009 I think? ( not working anymore - new-dmoz.lejer.ro)
+<br />
+Blog.Lejer.Ro <br/>
+Lot of, that i don't feel like pointing it here.... or beeing closed 
+projects, as i been testing a lot of frameworks.
+</div></div></div></div>
+</body>
+</html>
